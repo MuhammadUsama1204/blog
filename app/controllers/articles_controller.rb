ec47ae7229d91fcac089a0b49df56a8eec3c5d58
muhammadusama1204 <muhammad.usama@xprolabs.com>
2023-08-28 14:26:23 +0500

    default url option pratice

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,9 @@
 class ArticlesController < ApplicationController
   http_basic_authenticate_with name: "usama", password: "123", except: [:index, :show]
-
+  def default_url_options
+    { locale: I18n.locale }
+  end
+  
   def index
     @articles = Article.all
   end
