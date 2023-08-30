b7ee64383fedb97ef6e55403e8a3c8873e8c99c9
MuhammadUsama1204 <Muhammad.Usama@xprolabs.com>
2023-08-30 17:05:34 +0500

    add client model with curd operation

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,9 @@
+class ProfilesController < ApplicationController
+    def show
+    end
+    private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_client
+      @client = Client.find(params[:id])
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
+  resources :clients
   
   root 'articles#index'
   get '/sessions/new', to: "sessions#new"
   get 'controllers/sessions', to: "sessions#create"
   
+ 
+  resource :profile 
+  resources :users
+  
+  
+  resolve("Client") { [:profile] }
+  
+ 
   resources :articles do
     resources :comments
   end
