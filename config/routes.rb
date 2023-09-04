5d268cc6bea5d9cc41228206bade1fe65afa49d9
MuhammadUsama1204 <Muhammad.Usama@xprolabs.com>
2023-09-04 11:37:58 +0500

    add column in Person model name and age

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,21 @@
 Rails.application.routes.draw do
   resources :clients
   
-  root 'articles#index'
-  get '/sessions/new', to: "sessions#new"
-  get 'controllers/sessions', to: "sessions#create"
+   root 'articles#index'
+  # get '/sessions/new', to: "sessions#new"
+  # get 'controllers/sessions', to: "sessions#create"
   
  
-  resource :profile 
-  resources :users
+  # resource :profile 
+  # resources :users
   
   
-  resolve("Client") { [:profile] }
+  # resolve("Client") { [:profile] }
   
  
   resources :articles do
     resources :comments
+
+  resources:people
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_30_074524) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_04_063642) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -34,6 +34,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_30_074524) do
     t.datetime "updated_at", null: false
     t.string "status"
     t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
+  create_table "people", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "name"
+    t.integer "age"
   end
 
   create_table "profiles", force: :cascade do |t|
