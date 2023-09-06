62c0c2d08139e26ad563e271afab5fad18b873b3
MuhammadUsama1204 <Muhammad.Usama@xprolabs.com>
2023-09-06 19:08:45 +0500

    user:rename column email conf

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_06_132054) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_06_134813) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -56,7 +56,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_06_132054) do
     t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "confirmation_email"
+    t.string "email_confirmation"
   end
 
   add_foreign_key "comments", "articles"
