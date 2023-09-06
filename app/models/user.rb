a99530661d34630de33c45538f856e0b48b12fbc
MuhammadUsama1204 <Muhammad.Usama@xprolabs.com>
2023-09-06 19:08:07 +0500

    user:add some validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+validates :email, format: { with: /\A[a-zA-Z]+\z/,message: "only allows letters" }
+
+  validates :password, presence: true, length: { in: 4..10 }
+
+  #validates :my_email_attribute,email: true
+
+  #validates :email,confirmation: true
+  validates :email, confirmation: { case_sensitive: false }
+  validates :email_confirmation, presence: true
+
 end
