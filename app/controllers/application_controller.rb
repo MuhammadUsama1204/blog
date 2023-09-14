9d9a5035daf2f2e4056f98255e5d605972f19cff
MuhammadUsama1204 <Muhammad.Usama@xprolabs.com>
2023-09-14 17:30:32 +0500

    admin_user:controllers created

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,10 @@
 class ApplicationController < ActionController::Base
-    http_basic_authenticate_with name: "usama", password: "123", except: [:index, :show]
+    #http_basic_authenticate_with name: "usama", password: "123", except: [:index, :show]
     layout 'admin'
-   
+    before_action :authenticate_admin_user!
+    protected
+      def  after_sign_in_path_for(resource)
+        people_path
+      end
+
 end
-
-
